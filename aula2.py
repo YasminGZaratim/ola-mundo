aa37d4b37d573f86de45d4fdf0166352b3b74f77
Yasmin Zaratim <135038867+YasminGZaratim@users.noreply.github.com>
2024-08-15 09:43:41 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,57 @@
+#print("teste")
+#nome = "Pedro"
+#sobrenome = "Silva"
+#nomeCompleto = nome + " " + sobrenome
+#print(nomeCompleto)
+#print(nome, sobrenome)
+#print("Seu nome completo:", nome, sobrenome)
+#nome = input("Digite seu nome: ")
+#sobrenome = input("Digite seu sobrenome: ")
+#print("Seu nome completo:", nome, sobrenome)
+
+#n1 = 2
+#n2 = 3
+#n1 = int(input("Digite o 1º número: "))
+#n2 = int(input("Digite o 2º número: "))
+#resultado = (n1 + n2) ** 2
+#print(resultado)
+
+#1º questão
+#print("Olá mundo!")
+
+#2º questão
+#n1 = int(input("Digite o 1º número: "))
+#n2 = int(input("Digite o 2º número: "))
+#n3 = int(input("Digite o 3º número: "))
+#resultado = (n1 + n2 + n3)
+#print(resultado)
+
+#3º questão
+#fruta = input("Digite o nome da fruta: ")
+#print("A fruta digitada foi:", fruta)
+
+#4º questão
+#KM = float(input("Digite a distância em kilometro de uma cidade: "))
+#metros = KM * 1000
+#print("A distância em metros é:", metros)
+
+#5º questão
+nota1 = float(input("Digite a 1º nota: "))
+nota2 = float(input("Digite a 2º nota: "))
+media = (nota1 + nota2) / 2
+print(media)
+
+#6º questão
+lado = int(input("Digite o lado do quadrado: "))
+areaQuadrado = lado ** 2
+print(areaQuadrado)
+
+#7º questão
+raio = float(input("Digite o raio do circulo: "))
+areaCirculo = (raio * 3.14) ** 2
+print(areaCirculo)
+
+#8º questão
+temperaturaF = float(input("Digite a temperatura em Fahrenheit: "))
+temperaturaC = ((temperaturaF - 32) / 9) * 5
+print(temperaturaC)
